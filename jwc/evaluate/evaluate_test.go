baaf6fda8875a5c9dff5491821fbe98c633858f3
mohuishou <1@lailin.xyz>
2018-08-30 01:02:04 +0800

    评教适配成功！

--- jwc/evaluate/evaluate_test.go
+++ jwc/evaluate/evaluate_test.go
@@ -2,16 +2,22 @@ package evaluate
 
 import (
 	"testing"
+
 	"github.com/mohuishou/scu/jwc"
 	"github.com/mohuishou/scu/test"
 )
 
 func TestGetEvaList(t *testing.T) {
-	c, _ := jwc.Login(test.StudentID, test.Password)
-	res,err:=GetEvaList(c)
-	t.Log(len(res),err,res)
-	//r := res[len(res)-1]
-	//r.Comment = "超级棒的老师"
-	//r.Star = 5
-	//t.Log(AddEvaluate(c,&r))
+	c, _ := jwc.Login(test.JwcStudentID, test.JwcPassword)
+	res, err := GetEvaList(c)
+	if err != nil {
+		panic(err)
+	}
+
+	// t.Log(len(res), err, res)
+	c, _ = jwc.Login(test.JwcStudentID, test.JwcPassword)
+	r := res[len(res)-2]
+	r.Comment = "超级棒的老师"
+	r.Star = 5
+	t.Log(AddEvaluate(c, &r))
 }
